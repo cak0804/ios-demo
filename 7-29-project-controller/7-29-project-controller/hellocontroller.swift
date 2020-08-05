2cd0cbd7a971f24c32e7f75c8407315d3db01691
cak <1137051592@qq.com>
2020-08-05 17:27:09 +0800

    new file

---
+++ 7-29-project-controller/7-29-project-controller/hellocontroller.swift
@@ -0,0 +1,39 @@
+//
+//  hellocontroller.swift
+//  7-29-project-controller
+//
+//  Created by bytedance on 7/29/20.
+//
+
+import UIKit
+
+class hellocontroller: UIViewController {
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+//            let button = UIButton()
+//            button.frame = CGRect(x: 50, y: 80, width: 100, height: 45)
+//            button.setTitle("Back", for: .normal)
+//            button.backgroundColor = UIColor.cyan
+//            button.addTarget(self, action: #selector(back), for: .touchUpInside)
+//
+//            view.addSubview(button)
+//            view.backgroundColor = UIColor.white
+////
+////            print("second page")
+//        let button2 = UIButton(frame: CGRect(x: 200, y: 200, width: 100, height: 50))
+//        button2.backgroundColor = UIColor.black
+//        button2.setTitle("tonext", for: .normal)
+//        button2.addTarget(self, action: #selector(tonext), for: UIControl.Event.allEvents)
+//        view.addSubview(button2)
+
+        }
+
+//    @objc func tonext(){
+//        self.navigationController?.pushViewController(lastcontroller(), animated: true)
+//    }
+//        @objc func back() {
+//            // 返回上一页
+//            self.navigationController?.popViewController(animated: false)
+//        }
+}
